51eed4b590f5886c77cee1e66141de4e63c56c17
Muhsin Hameed <107370156+Id-Analyst@users.noreply.github.com>
2025-02-03 07:42:53 +0000

    Add files via upload

---
+++ 1. Band Name Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Brand Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet name?\n")
+brand_name = city + ' ' + pet_name
+print("Your band name could be " + brand_name)
